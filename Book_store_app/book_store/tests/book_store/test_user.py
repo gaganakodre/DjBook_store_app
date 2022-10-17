ac5ed1e2c56390533e3868b7c28c53364b84501f
shreegowri <gaganandh@gmail.com>
2022-10-17 17:09:13 +0530

    [shreegowri]pytest

---
+++ Book_store_app/book_store/tests/book_store/test_user.py
@@ -0,0 +1,20 @@
+import pytest
+from django.urls import reverse
+
+pytest_mark = pytest.mark.django_db
+REGISTER_URL = reverse('register')
+
+@pytest.fixture
+def user_data():
+    return {'username': 'gagana',
+            'first_name': 'gaga', 'last_name': 'na',
+            'email': 'gagana@gmail.com',
+            'password': 'gagana',
+            'location':"bng",
+            "phone_number":1233}
+
+class TestUserLoginAndRegister:
+    @pytest.mark.django_db
+    def test_user_registration_successfully(self, client, django_user_model, user_data):
+        response = client.post(REGISTER_URL, user_data, content_type="application/json")
+        assert response.status_code == 200
